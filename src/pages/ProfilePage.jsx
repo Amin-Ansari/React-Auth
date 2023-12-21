c8fd0c9c9b124fc4ec849fd20245a07ea6e60d35
Amin Ansari <devansari@yahoo.com>
2023-12-21 20:02:06 +0330

    The profile page layout is done

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,3 +1,24 @@
-const ProfilePage = () => {};
+import { Form } from "react-router-dom";
+import "../utilities/ProfilePage.css";
+import "../utilities/AuthForm.css";
+
+const ProfilePage = () => {
+  return (
+    <>
+      <h2 className="profile-page-title ">Your User Profile</h2>
+      <Form method="POST" className="auth-form change-password-form">
+        <label className="form-label">New Password</label>
+        <input
+          type="password"
+          name="new-password"
+          className="auth-form-input pass-change-input"
+        ></input>
+        <button type="submit" className="change-the-pass-button">
+          Change the Password
+        </button>
+      </Form>
+    </>
+  );
+};
 
 export default ProfilePage;
